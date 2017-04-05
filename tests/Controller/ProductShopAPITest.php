dd9efb282226291503ddc61d405b0a3e41df84e8
Łukasz <lchrusciel@gmail.com>
2017-04-05 11:25:21 +0200

    [Design] PDP + PLP requests

---
+++ tests/Controller/ProductShopAPITest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Acme\ExampleBundle\Controller;
+
+use Lakion\ApiTestCase\JsonApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ProductShopAPITest extends JsonApiTestCase
+{
+    public function it_shows_simple_product_details_page()
+    {
+        $this->client->request('GET', '/shop-api/products/logan-mug?channel=WEB-GB', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/simple_product_details_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_simple_product_details_page_in_different_locale()
+    {
+        $this->client->request('GET', '/shop-api/products/logan-mug?channel=WEB-GB&locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/simple_product_details_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_product_with_variant_details_page()
+    {
+        $this->client->request('GET', '/shop-api/products/logan-t-shirt?channel=WEB-GB', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/simple_product_details_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_product_with_variant_details_page_in_different_locale()
+    {
+        $this->client->request('GET', '/shop-api/products/logan-t-shirt?channel=WEB-GB&locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/simple_product_details_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_product_with_options_details_page()
+    {
+        $this->client->request('GET', '/shop-api/products/logan-hat?channel=WEB-GB', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/simple_product_details_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_product_with_options_details_page_in_different_locale()
+    {
+        $this->client->request('GET', '/shop-api/products/logan-hat?channel=WEB-GB&locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/simple_product_details_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_paginated_products_from_some_taxon()
+    {
+        $this->client->request('GET', '/shop-api/taxons/x-man/products/?channel=WEB-GB', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_paginated_products_from_some_taxon_in_different_language()
+    {
+        $this->client->request('GET', '/shop-api/taxons/x-man/products/?channel=WEB-GB&language=de_DE', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_second_page_of_paginated_products_from_some_taxon()
+    {
+        $this->client->request('GET', '/shop-api/taxons/x-man/products/?channel=WEB-GB&limit=1&page=2', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+    }
+
+    public function it_shows_sorted_product_list()
+    {
+        $this->client->request('GET', '/shop-api/taxons/x-man/products/?channel=WEB-GB&sorting[createdAt]=desc', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+    }
+
+    public function it_expose_only_some_of_products_in_the_list()
+    {
+        $this->client->request('GET', '/shop-api/taxons/x-man/products/?channel=WEB-GB&criteria[search][value]=Logans+Hat', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+    }
+}
