cb6ba54eea022f02d5f41b96067ee1e8313884af
Łukasz <lchrusciel@gmail.com>
2017-04-19 12:35:07 +0200

    [Taxon] Test for translatables

--- tests/Controller/TaxonShopAPITest.php
+++ tests/Controller/TaxonShopAPITest.php
@@ -23,6 +23,19 @@ final class TaxonShopAPITest extends JsonApiTestCase
     /**
      * @test
      */
+    public function it_shows_summary_of_a_chosen_taxon_in_different_language()
+    {
+        $this->loadFixturesFromFile('shop.yml');
+
+        $this->client->request('GET', '/shop-api/taxons/t-shirts?locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'taxon/german_one_of_taxons_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
     public function it_shows_tree_of_all_taxons()
     {
         $this->loadFixturesFromFile('shop.yml');
@@ -32,5 +45,19 @@ final class TaxonShopAPITest extends JsonApiTestCase
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'taxon/all_taxons_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_tree_of_all_taxons_in_different_language()
+    {
+        $this->loadFixturesFromFile('shop.yml');
+
+        $this->client->request('GET', '/shop-api/taxons/?locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'taxon/german_all_taxons_response', Response::HTTP_OK);
     }
 }
