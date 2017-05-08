8c364da56b2e4abdea88178a72441e7b42478837
Łukasz Chruściel <lchrusciel@gmail.com>
2017-05-08 13:34:58 +0200

    [Checkout] Make controllers more SOLID

---
+++ src/Controller/Checkout/AddressAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Sylius\ShopApiPlugin\Controller\Checkout;
+
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\View\ViewHandlerInterface;
+use League\Tactician\CommandBus;
+use Sylius\ShopApiPlugin\Command\AddressOrder;
+use Sylius\ShopApiPlugin\Model\Address;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class AddressAction
+{
+    /**
+     * @var ViewHandlerInterface
+     */
+    private $viewHandler;
+
+    /**
+     * @var CommandBus
+     */
+    private $bus;
+
+    /**
+     * @param ViewHandlerInterface $viewHandler
+     * @param CommandBus $bus
+     */
+    public function __construct(ViewHandlerInterface $viewHandler, CommandBus $bus)
+    {
+        $this->viewHandler = $viewHandler;
+        $this->bus = $bus;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request)
+    {
+        $this->bus->handle(AddressOrder::create(
+            $request->attributes->get('token'),
+            Address::createFromArray($request->request->get('shippingAddress')),
+            Address::createFromArray(
+                $request->request->has('billingAddress') ?
+                    $request->request->get('billingAddress') :
+                    $request->request->get('shippingAddress')
+            )
+        ));
+
+        return $this->viewHandler->handle(View::create(null, Response::HTTP_NO_CONTENT));
+    }
+}
