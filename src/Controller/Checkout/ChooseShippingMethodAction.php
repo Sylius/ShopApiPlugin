c2839436f23361fe34418382158371a594a849a2
Łukasz Chruściel <lchrusciel@gmail.com>
2017-05-09 10:03:21 +0200

    [Checkout] Extract shipping method view factory

--- src/Controller/Checkout/ChooseShippingMethodAction.php
+++ src/Controller/Checkout/ChooseShippingMethodAction.php
@@ -5,24 +5,9 @@ namespace Sylius\ShopApiPlugin\Controller\Checkout;
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandlerInterface;
 use League\Tactician\CommandBus;
-use Sylius\Component\Core\Model\OrderInterface;
-use Sylius\Component\Core\Model\ShipmentInterface;
-use Sylius\Component\Core\Model\ShippingMethodInterface;
-use Sylius\Component\Core\Repository\OrderRepositoryInterface;
-use Sylius\Component\Registry\ServiceRegistryInterface;
-use Sylius\Component\Shipping\Calculator\CalculatorInterface;
-use Sylius\Component\Shipping\Resolver\ShippingMethodsResolverInterface;
-use Sylius\ShopApiPlugin\Builder\AddressViewFactoryInterface;
-use Sylius\ShopApiPlugin\Builder\CartViewFactoryInterface;
-use Sylius\ShopApiPlugin\Command\AddressOrder;
 use Sylius\ShopApiPlugin\Command\ChooseShippingMethod;
-use Sylius\ShopApiPlugin\Model\Address;
-use Sylius\ShopApiPlugin\View\AddressView;
-use Sylius\ShopApiPlugin\View\ShipmentMethodView;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class ChooseShippingMethodAction
 {
---
+++ src/Factory/ShippingMethodViewFactoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Sylius\ShopApiPlugin\Factory;
+
+use Sylius\Component\Core\Model\ShipmentInterface;
+use Sylius\Component\Core\Model\ShippingMethodInterface;
+use Sylius\ShopApiPlugin\View\ShippingMethodView;
+
+interface ShippingMethodViewFactoryInterface
+{
+    /**
+     * @param ShipmentInterface $shipment
+     * @param ShippingMethodInterface $shippingMethod
+     * @param string $locale
+     *
+     * @return ShippingMethodView
+     */
+    public function create(ShipmentInterface $shipment, ShippingMethodInterface $shippingMethod, $locale);
+}
