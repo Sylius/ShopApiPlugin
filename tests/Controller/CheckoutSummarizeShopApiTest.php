258cd12cb6b847f53d8d143fee2b8e1894860519
Łukasz Chruściel <lchrusciel@gmail.com>
2017-05-11 09:53:47 +0200

    [Maintenance] Remove unused parameter

--- tests/Controller/CheckoutSummarizeShopApiTest.php
+++ tests/Controller/CheckoutSummarizeShopApiTest.php
@@ -43,7 +43,7 @@ EOT;
 
         $this->client->request('PUT', sprintf('/shop-api/checkout/%s/address', $token), [], [], static::$acceptAndContentTypeHeader, $data);
 
-        $this->client->request('GET', '/shop-api/checkout/' . $token, [], [], static::$acceptAndContentTypeHeader, $data);
+        $this->client->request('GET', '/shop-api/checkout/' . $token, [], [], static::$acceptAndContentTypeHeader);
 
         $response = $this->client->getResponse();
         $this->assertResponse($response, 'checkout/cart_addressed_response', Response::HTTP_OK);
@@ -89,7 +89,7 @@ EOT;
 
         $this->client->request('PUT', sprintf('/shop-api/checkout/%s/address', $token), [], [], static::$acceptAndContentTypeHeader, $data);
 
-        $this->client->request('GET', '/shop-api/checkout/' . $token, [], [], static::$acceptAndContentTypeHeader, $data);
+        $this->client->request('GET', '/shop-api/checkout/' . $token, [], [], static::$acceptAndContentTypeHeader);
 
         $response = $this->client->getResponse();
         $this->assertResponse($response, 'checkout/cart_addressed_with_different_shipping_and_billing_address_response', Response::HTTP_OK);
