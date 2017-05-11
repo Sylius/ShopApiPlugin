e2b2dadda5c6c9bf5daef09857935392fcc19430
Łukasz Chruściel <lchrusciel@gmail.com>
2017-05-11 12:15:41 +0200

    Remove unneded test

--- tests/Controller/CheckoutChooseShippingMethodApiTest.php
+++ tests/Controller/CheckoutChooseShippingMethodApiTest.php
@@ -12,14 +12,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 final class CheckoutChooseShippingMethodApiTest extends JsonApiTestCase
 {
-    public function it_does_not_provide_details_about_available_shipping_method_for_unexisting_cart()
-    {
-        $this->client->request('GET', $this->getShippingUrl(0), [], []);
-
-        $response = $this->client->getResponse();
-        $this->assertResponseCode($response, Response::HTTP_NOT_FOUND);
-    }
-
     /**
      * @test
      */
