1c4dc9521752bb9c7fc394cf9f6966923014ab32
Łukasz Chruściel <lchrusciel@gmail.com>
2017-05-29 15:00:00 +0200

    [Maintenance] Add strict type declaration

--- src/Factory/ProductAttributeValueViewFactory.php
+++ src/Factory/ProductAttributeValueViewFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Sylius\ShopApiPlugin\Factory;
 
 use Sylius\Component\Product\Model\ProductAttributeValueInterface;
--- tests/Request/PutVariantBasedConfigurableItemToCartRequestTest.php
+++ tests/Request/PutVariantBasedConfigurableItemToCartRequestTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Sylius\ShopApiPlugin\Request;
 
 use Sylius\ShopApiPlugin\Command\PutVariantBasedConfigurableItemToCart;
