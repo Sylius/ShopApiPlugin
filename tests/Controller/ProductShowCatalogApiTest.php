29193b076bbbce923472d6c8453d0fa74096527e
Łukasz Chruściel <lchrusciel@gmail.com>
2017-06-22 12:19:54 +0200

    [Product] Make product catalog path not collide with taxon details

--- tests/Controller/ProductShowCatalogApiTest.php
+++ tests/Controller/ProductShowCatalogApiTest.php
@@ -14,7 +14,7 @@ final class ProductShowCatalogApiTest extends JsonApiTestCase
     {
         $this->loadFixturesFromFile('shop.yml');
 
-        $this->client->request('GET', '/shop-api/taxons/brands/products/?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
+        $this->client->request('GET', '/shop-api/taxon-products/brands?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
@@ -27,7 +27,7 @@ final class ProductShowCatalogApiTest extends JsonApiTestCase
     {
         $this->loadFixturesFromFile('shop.yml');
 
-        $this->client->request('GET', '/shop-api/taxons/categories/t-shirts/products/?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
+        $this->client->request('GET', '/shop-api/taxon-products/categories/t-shirts?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/product_t_shirt_list_page', Response::HTTP_OK);
@@ -40,7 +40,7 @@ final class ProductShowCatalogApiTest extends JsonApiTestCase
     {
         $this->loadFixturesFromFile('shop.yml');
 
-        $this->client->request('GET', '/shop-api/taxons/marken/products/?channel=WEB_GB&locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
+        $this->client->request('GET', '/shop-api/taxon-products/marken?channel=WEB_GB&locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/german_product_list_page', Response::HTTP_OK);
@@ -53,7 +53,7 @@ final class ProductShowCatalogApiTest extends JsonApiTestCase
     {
         $this->loadFixturesFromFile('shop.yml');
 
-        $this->client->request('GET', '/shop-api/taxons/brands/products/?channel=WEB_GB&limit=1&page=2', [], [], ['ACCEPT' => 'application/json']);
+        $this->client->request('GET', '/shop-api/taxon-products/brands?channel=WEB_GB&limit=1&page=2', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/limited_product_list_page', Response::HTTP_OK);
@@ -61,7 +61,7 @@ final class ProductShowCatalogApiTest extends JsonApiTestCase
 
     public function it_shows_sorted_product_list()
     {
-        $this->client->request('GET', '/shop-api/taxons/x-man/products/?channel=WEB_GB&sorting[createdAt]=desc', [], [], ['ACCEPT' => 'application/json']);
+        $this->client->request('GET', '/shop-api/taxon-products/x-man?channel=WEB_GB&sorting[createdAt]=desc', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
@@ -69,7 +69,7 @@ final class ProductShowCatalogApiTest extends JsonApiTestCase
 
     public function it_expose_only_some_of_products_in_the_list()
     {
-        $this->client->request('GET', '/shop-api/taxons/x-man/products/?channel=WEB_GB&criteria[search][value]=Logans+Hat', [], [], ['ACCEPT' => 'application/json']);
+        $this->client->request('GET', '/shop-api/taxon-products/x-man?channel=WEB_GB&criteria[search][value]=Logans+Hat', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
