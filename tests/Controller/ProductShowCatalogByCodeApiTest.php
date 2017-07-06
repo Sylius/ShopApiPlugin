ce1ded771d9b7830640ad02b2dd747374090ac29
Łukasz Chruściel <lchrusciel@gmail.com>
2017-07-06 18:12:02 +0200

    [Products] Show product catalog by taxon code

---
+++ tests/Controller/ProductShowCatalogByCodeApiTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Sylius\ShopApiPlugin\Controller;
+
+use Lakion\ApiTestCase\JsonApiTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ProductShowCatalogByCodeApiTest extends JsonApiTestCase
+{
+    /**
+     * @test
+     */
+    public function it_shows_paginated_products_from_some_taxon_by_code()
+    {
+        $this->loadFixturesFromFile('shop.yml');
+
+        $this->client->request('GET', '/shop-api/taxon-products/BRAND?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_page_by_code_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_products_for_sub_taxons_by_code()
+    {
+        $this->loadFixturesFromFile('shop.yml');
+
+        $this->client->request('GET', '/shop-api/taxon-products/WOMEN_T_SHIRTS?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_t_shirt_list_page_by_code_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_paginated_products_from_some_taxon_by_code_in_different_language()
+    {
+        $this->loadFixturesFromFile('shop.yml');
+
+        $this->client->request('GET', '/shop-api/taxon-products/BRAND?channel=WEB_GB&locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/german_product_list_page_by_code_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_second_page_of_paginated_products_from_some_taxon_by_code()
+    {
+        $this->loadFixturesFromFile('shop.yml');
+
+        $this->client->request('GET', '/shop-api/taxon-products/BRAND?channel=WEB_GB&limit=1&page=2', [], [], ['ACCEPT' => 'application/json']);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/limited_product_list_page_by_code_response', Response::HTTP_OK);
+    }
+}
--- tests/Controller/ProductShowCatalogBySlugApiTest.php
+++ tests/Controller/ProductShowCatalogBySlugApiTest.php
@@ -17,7 +17,7 @@ final class ProductShowCatalogBySlugApiTest extends JsonApiTestCase
         $this->client->request('GET', '/shop-api/taxon-products-by-slug/brands?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
-        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+        $this->assertResponse($response, 'product/product_list_page_by_slug_response', Response::HTTP_OK);
     }
 
     /**
@@ -30,7 +30,7 @@ final class ProductShowCatalogBySlugApiTest extends JsonApiTestCase
         $this->client->request('GET', '/shop-api/taxon-products-by-slug/categories/t-shirts/women-t-shirts?channel=WEB_GB', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
-        $this->assertResponse($response, 'product/product_t_shirt_list_page', Response::HTTP_OK);
+        $this->assertResponse($response, 'product/product_t_shirt_list_page_by_slug_response', Response::HTTP_OK);
     }
 
     /**
@@ -43,7 +43,7 @@ final class ProductShowCatalogBySlugApiTest extends JsonApiTestCase
         $this->client->request('GET', '/shop-api/taxon-products-by-slug/marken?channel=WEB_GB&locale=de_DE', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
-        $this->assertResponse($response, 'product/german_product_list_page', Response::HTTP_OK);
+        $this->assertResponse($response, 'product/german_product_list_page_by_slug_response', Response::HTTP_OK);
     }
 
     /**
@@ -56,7 +56,7 @@ final class ProductShowCatalogBySlugApiTest extends JsonApiTestCase
         $this->client->request('GET', '/shop-api/taxon-products-by-slug/brands?channel=WEB_GB&limit=1&page=2', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
-        $this->assertResponse($response, 'product/limited_product_list_page', Response::HTTP_OK);
+        $this->assertResponse($response, 'product/limited_product_list_page_by_slug_response', Response::HTTP_OK);
     }
 
     public function it_shows_sorted_product_list()
@@ -64,7 +64,7 @@ final class ProductShowCatalogBySlugApiTest extends JsonApiTestCase
         $this->client->request('GET', '/shop-api/taxon-products-by-slug/x-man?channel=WEB_GB&sorting[createdAt]=desc', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
-        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+        $this->assertResponse($response, 'product/product_list_page_by_slug_response', Response::HTTP_OK);
     }
 
     public function it_expose_only_some_of_products_in_the_list()
@@ -72,6 +72,6 @@ final class ProductShowCatalogBySlugApiTest extends JsonApiTestCase
         $this->client->request('GET', '/shop-api/taxon-products-by-slug/x-man?channel=WEB_GB&criteria[search][value]=Logans+Hat', [], [], ['ACCEPT' => 'application/json']);
         $response = $this->client->getResponse();
 
-        $this->assertResponse($response, 'product/product_list_page', Response::HTTP_OK);
+        $this->assertResponse($response, 'product/product_list_page_by_slug_response', Response::HTTP_OK);
     }
 }
