89c012697cbb4293119cf4e8440ca5acd346cfaa
Kamil Kokot <kamil@kokot.me>
2017-09-27 16:26:15 +0200

    Manual coding standard fixes

--- src/Model/PaginatorDetails.php
+++ src/Model/PaginatorDetails.php
@@ -23,8 +23,8 @@ final class PaginatorDetails
     public function __construct(string $route, array $parameters)
     {
         $this->route = $route;
-        $this->limit = $parameters['limit'] ?? 10;
-        $this->page = $parameters['page'] ?? 1;
+        $this->limit = (int) ($parameters['limit'] ?? 10);
+        $this->page = (int) ($parameters['page'] ?? 1);
         $this->parameters = $parameters;
     }
 
--- tests/Application/app/autoload.php
+++ tests/Application/app/autoload.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 use Composer\Autoload\ClassLoader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
