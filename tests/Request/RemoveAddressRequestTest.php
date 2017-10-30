e3b109a77508103a83f7121b0309ec1c5084b44d
Jurica Šeparović <jsepar00@fesb.hr>
2017-10-30 15:52:17 +0100

    [Address-book] Remove address request test

--- tests/Request/RemoveAddressRequestTest.php
+++ tests/Request/RemoveAddressRequestTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Sylius\ShopApiPlugin\Controller\AddressBook;
 
 use PHPUnit\Framework\TestCase;
+use Sylius\ShopApiPlugin\Command\RemoveAddress;
+use Sylius\ShopApiPlugin\Request\RemoveAddressRequest;
+use Symfony\Component\HttpFoundation\Request;
 
 class RemoveAddressRequestTest extends TestCase
 {
@@ -13,5 +16,8 @@ class RemoveAddressRequestTest extends TestCase
      */
     public function it_creates_remove_address_command()
     {
+        $removeAddressRequest = new RemoveAddressRequest(new Request([], [], ['id' => '1']));
+
+        $this->assertEquals($removeAddressRequest->getCommand(), new RemoveAddress('1'));
     }
 }
