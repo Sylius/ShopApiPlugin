1a54ec2a6138dbd73c9fa57527d19fecc5e0d336
Lei Wang <wang@solutiondrive.de>
2017-11-04 14:28:20 +0100

    add api test for latest 2 product

--- tests/Controller/ProductShowLatestApiTest.php
+++ tests/Controller/ProductShowLatestApiTest.php
@@ -22,3 +22,18 @@ final class ProductShowLatestApiTest extends JsonApiTestCase
 
         $this->assertResponse($response, 'product/product_list_latest_4_response', Response::HTTP_OK);
     }
+
+    /**
+     * @test
+     */
+    public function it_shows_latest_2_products()
+    {
+        $this->loadFixturesFromFile('shop.yml');
+
+        $this->client->request('GET', '/shop-api/product-latest/', ['channel' => 'WEB_GB', 'limit' => 2], [], ['ACCEPT' => 'application/json']);
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_latest_2_response', Response::HTTP_OK);
+    }
+}
