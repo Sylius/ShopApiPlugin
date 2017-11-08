c368f40c14f0397cdc84bacdcd0462cb71c65f31
Lei Wang <wang@solutiondrive.de>
2017-11-08 20:28:45 +0100

    add final

--- src/ViewRepository/ProductLatestViewRepository.php
+++ src/ViewRepository/ProductLatestViewRepository.php
@@ -12,7 +12,7 @@ use Sylius\ShopApiPlugin\Factory\ProductViewFactoryInterface;
 use Sylius\ShopApiPlugin\View\ProductListView;
 use Webmozart\Assert\Assert;
 
-class ProductLatestViewRepository implements ProductLatestViewRepositoryInterface
+final class ProductLatestViewRepository implements ProductLatestViewRepositoryInterface
 {
     /** @var ChannelRepositoryInterface */
     private $channelRepository;
