e6d9dd8350b4342b8f6108c035d8c59d64fb3dc5
Mateusz Zalewski <zaleslaw@gmail.com>
2018-10-04 13:55:18 +0200

    Placed order view implementation

---
+++ src/View/PlacedOrderView.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\ShopApiPlugin\View;
+
+class PlacedOrderView
+{
+    /** @var string */
+    public $channel;
+
+    /** @var string */
+    public $currency;
+
+    /** @var string */
+    public $locale;
+
+    /** @var string */
+    public $checkoutState;
+
+    /** @var string */
+    public $state;
+
+    /** @var array|ItemView[] */
+    public $items = [];
+
+    /** @var TotalsView */
+    public $totals;
+
+    /** @var AddressView */
+    public $shippingAddress;
+
+    /** @var AddressView */
+    public $billingAddress;
+
+    /** @var array|PaymentView[] */
+    public $payments = [];
+
+    /** @var array|ShipmentView[] */
+    public $shipments = [];
+
+    /** @var array|AdjustmentView[] */
+    public $cartDiscounts = [];
+
+    public function __construct()
+    {
+        $this->totals = new TotalsView();
+    }
+}
