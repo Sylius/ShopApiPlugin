ca91ca3860198438fa50a9d5b69ba29ed03c5960
Łukasz Chruściel <lchrusciel@gmail.com>
2019-01-25 20:49:58 +0100

    [POC] New command providers

--- src/Controller/Cart/DropCartAction.php
+++ src/Controller/Cart/DropCartAction.php
@@ -6,12 +6,11 @@ namespace Sylius\ShopApiPlugin\Controller\Cart;
 
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandlerInterface;
+use Sylius\ShopApiPlugin\CommandProvider\CommandProviderInterface;
 use Sylius\ShopApiPlugin\Factory\ValidationErrorViewFactoryInterface;
-use Sylius\ShopApiPlugin\Request\Cart\DropCartRequest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 final class DropCartAction
 {
@@ -21,32 +20,30 @@ final class DropCartAction
     /** @var MessageBusInterface */
     private $bus;
 
-    /** @var ValidatorInterface */
-    private $validator;
-
     /** @var ValidationErrorViewFactoryInterface */
     private $validationErrorViewFactory;
+
+    /** @var CommandProviderInterface */
+    private $dropCartCommandProvider;
 
     public function __construct(
         ViewHandlerInterface $viewHandler,
         MessageBusInterface $bus,
-        ValidatorInterface $validator,
-        ValidationErrorViewFactoryInterface $validationErrorViewFactory
+        ValidationErrorViewFactoryInterface $validationErrorViewFactory,
+        CommandProviderInterface $dropCartCommandProvider
     ) {
         $this->viewHandler = $viewHandler;
         $this->bus = $bus;
-        $this->validator = $validator;
         $this->validationErrorViewFactory = $validationErrorViewFactory;
+        $this->dropCartCommandProvider = $dropCartCommandProvider;
     }
 
     public function __invoke(Request $request): Response
     {
-        $pickupRequest = new DropCartRequest($request);
-
-        $validationResults = $this->validator->validate($pickupRequest);
+        $validationResults = $this->dropCartCommandProvider->validate($request);
 
         if (0 === count($validationResults)) {
-            $this->bus->dispatch($pickupRequest->getCommand());
+            $this->bus->dispatch($this->dropCartCommandProvider->getCommand($request));
 
             return $this->viewHandler->handle(View::create(null, Response::HTTP_NO_CONTENT));
         }
