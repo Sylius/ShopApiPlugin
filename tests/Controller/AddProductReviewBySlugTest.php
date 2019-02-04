227fbf8e394de9ccd4a9d4c418ba82acd031a6ff
mamazu <maxo.pech@gmail.com>
2019-02-04 10:48:53 +0100

    Added validation for product review

---
+++ tests/Controller/AddProductReviewBySlugTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\ShopApiPlugin\Controller\Product;
+
+use PHPUnit\Framework\Assert;
+use Sylius\Component\Core\Model\AddressInterface;
+use Sylius\Component\Core\Model\Customer;
+use Sylius\Component\Core\Repository\AddressRepositoryInterface;
+use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Sylius\ShopApiPlugin\Controller\Utils\JsonApiTestCase;
+use Tests\Sylius\ShopApiPlugin\Controller\Utils\ShopUserLoginTrait;
+
+final class AddProductReviewBySlugTest extends JsonApiTestCase
+{
+    private static $acceptAndContentTypeHeader = ['CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'];
+
+    /**
+     * @test
+     */
+    public function fails_to_add_review_because_rating_is_out_of_bounds()
+    {
+        $this->loadFixturesFromFiles(['channel.yml', 'shop.yml']);
+
+        $data =
+<<<EOT
+        {
+            "comment": "Hello",
+            "rating": 100,
+            "email": "test@test.com",
+            "title": "Testing"
+        }
+EOT;
+
+        $this->client->request('POST', '/shop-api/WEB_GB/product-reviews-by-slug/mug', [], [], self::$acceptAndContentTypeHeader, $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'reviews/add_review_failed_rating', Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @test
+     */
+    public function fails_to_add_review_because_rating_email_is_not_valid()
+    {
+        $this->loadFixturesFromFiles(['channel.yml', 'shop.yml']);
+
+        $data =
+<<<EOT
+        {
+            "comment": "Hello",
+            "rating": 100,
+            "email": "test.com",
+            "title": "Testing"
+        }
+EOT;
+
+        $this->client->request('POST', '/shop-api/WEB_GB/product-reviews-by-slug/mug', [], [], self::$acceptAndContentTypeHeader, $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'reviews/add_review_failed_email', Response::HTTP_BAD_REQUEST);
+    }
+}
