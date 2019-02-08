98932c717f99aef59ff2a2be302ebe3b5a2fd90e
mamazu <maxo.pech@gmail.com>
2019-02-08 16:21:27 +0100

    Moved the tests to the correct controller

--- tests/Controller/ProductAddReviewByCodeApiTest.php
+++ tests/Controller/ProductAddReviewByCodeApiTest.php
@@ -18,7 +18,7 @@ final class ProductAddReviewByCodeApiTest extends JsonApiTestCase
         $this->loadFixturesFromFiles(['shop.yml']);
 
         $data =
-            <<<EOT
+<<<EOT
         {
             "title": "Awesome product",
             "rating": 5,
@@ -74,5 +74,53 @@ EOT;
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'channel_has_not_been_found_response', Response::HTTP_NOT_FOUND);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_allow_to_add_review_when_rating_is_out_of_bounds()
+    {
+        $this->loadFixturesFromFiles(['channel.yml', 'shop.yml']);
+
+        $data =
+<<<EOT
+        {
+            "comment": "Hello",
+            "rating": 100,
+            "email": "test@test.com",
+            "title": "Testing",
+            "code": "LOGAN_MUG_CODE"
+        }
+EOT;
+
+        $this->client->request('POST', '/shop-api/WEB_GB/products/LOGAN_MUG_CODE/reviews', [], [], self::$acceptAndContentTypeHeader, $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'reviews/add_review_failed_rating', Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_allow_to_add_review_when_email_is_not_valid()
+    {
+        $this->loadFixturesFromFiles(['channel.yml', 'shop.yml']);
+
+        $data =
+<<<EOT
+        {
+            "comment": "Hello",
+            "rating": 4,
+            "email": "test.com",
+            "title": "Testing",
+            "code": "LOGAN_MUG_CODE"
+        }
+EOT;
+
+        $this->client->request('POST', '/shop-api/WEB_GB/products/LOGAN_MUG_CODE/reviews', [], [], self::$acceptAndContentTypeHeader, $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'reviews/add_review_failed_email', Response::HTTP_BAD_REQUEST);
     }
 }
