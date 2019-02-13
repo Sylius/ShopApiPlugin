c2fbba2981be15171dcc4afd92a73714d621965b
mamazu <maxo.pech@gmail.com>
2019-02-13 16:31:00 +0100

    Addeding expected responses

--- tests/Controller/JsonApiTestCase.php
+++ tests/Controller/JsonApiTestCase.php
@@ -6,11 +6,11 @@ namespace Tests\Sylius\ShopApiPlugin\Controller;
 
 abstract class JsonApiTestCase extends \Lakion\ApiTestCase\JsonApiTestCase
 {
-
     public function __construct(?string $name = null, array $data = [], string $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
         $this->dataFixturesPath = __DIR__ . '/../DataFixtures/ORM';
+        $this->expectedResponsesPath = __DIR__ . '/../Responses/Expected';
     }
 
     protected function get($id)
