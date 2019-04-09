2128106190223d8a8657224a8bcda35ac6b6559a
Grzegorz Sadowski <sadowskigp@gmail.com>
2019-04-09 11:36:37 +0200

    [Tests] Enable tests for showing available payment and shipping methods in wrong state of checkout

--- src/Controller/Checkout/ShowAvailableShippingMethodsAction.php
+++ src/Controller/Checkout/ShowAvailableShippingMethodsAction.php
@@ -6,14 +6,17 @@ namespace Sylius\ShopApiPlugin\Controller\Checkout;
 
 use FOS\RestBundle\View\View;
 use FOS\RestBundle\View\ViewHandlerInterface;
+use SM\Factory\FactoryInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\ShipmentInterface;
 use Sylius\Component\Core\Model\ShippingMethodInterface;
+use Sylius\Component\Core\OrderCheckoutTransitions;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use Sylius\Component\Shipping\Resolver\ShippingMethodsResolverInterface;
 use Sylius\ShopApiPlugin\Factory\Checkout\ShippingMethodViewFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 final class ShowAvailableShippingMethodsAction
 {
@@ -29,16 +32,21 @@ final class ShowAvailableShippingMethodsAction
     /** @var ShippingMethodViewFactoryInterface */
     private $shippingMethodViewFactory;
 
+    /** @var FactoryInterface */
+    private $stateMachineFactory;
+
     public function __construct(
         OrderRepositoryInterface $cartRepository,
         ViewHandlerInterface $viewHandler,
         ShippingMethodsResolverInterface $shippingMethodsResolver,
-        ShippingMethodViewFactoryInterface $shippingMethodViewFactory
+        ShippingMethodViewFactoryInterface $shippingMethodViewFactory,
+        FactoryInterface $stateMachineFactory
     ) {
         $this->cartRepository = $cartRepository;
         $this->viewHandler = $viewHandler;
         $this->shippingMethodsResolver = $shippingMethodsResolver;
         $this->shippingMethodViewFactory = $shippingMethodViewFactory;
+        $this->stateMachineFactory = $stateMachineFactory;
     }
 
     public function __invoke(Request $request): Response
@@ -46,8 +54,11 @@ final class ShowAvailableShippingMethodsAction
         /** @var OrderInterface $cart */
         $cart = $this->cartRepository->findOneBy(['tokenValue' => $request->attributes->get('token')]);
 
-        $shipments = [];
+        if (!$this->isCheckoutTransitionPossible($cart, OrderCheckoutTransitions::TRANSITION_SELECT_SHIPPING)) {
+            throw new BadRequestHttpException('The shipment methods cannot be resolved in the current state of cart!');
+        }
 
+        $shipments = [];
         foreach ($cart->getShipments() as $shipment) {
             $shipments['shipments'][] = $this->getCalculatedShippingMethods($shipment, $cart->getLocaleCode());
         }
@@ -73,5 +84,10 @@ final class ShowAvailableShippingMethodsAction
         }
 
         return $rawShippingMethods;
+    }
+
+    private function isCheckoutTransitionPossible(OrderInterface $cart, string $transition): bool
+    {
+        return $this->stateMachineFactory->get($cart, OrderCheckoutTransitions::GRAPH)->can($transition);
     }
 }
