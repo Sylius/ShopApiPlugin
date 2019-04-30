41ce42f0c49f5d6781fe2046feef5bf8bd79711a
David Lobato <dav.lobato@gmail.com>
2019-04-30 17:30:25 +0100

    Fix tests

--- spec/Command/Order/UpdatePaymentMethodSpec.php
+++ spec/Command/Order/UpdatePaymentMethodSpec.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace spec\Sylius\ShopApiPlugin\Command\Order;
 
 use PhpSpec\ObjectBehavior;
-use TypeError;
 
 final class UpdatePaymentMethodSpec extends ObjectBehavior
 {
