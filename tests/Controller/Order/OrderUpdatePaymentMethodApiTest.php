175eb0702b84273a74ecab2ff75659633d7da2a1
David Lobato <dav.lobato@gmail.com>
2019-05-10 18:22:46 +0100

    Update tests

--- tests/Controller/Order/OrderUpdatePaymentMethodApiTest.php
+++ tests/Controller/Order/OrderUpdatePaymentMethodApiTest.php
@@ -6,19 +6,26 @@ namespace Tests\Sylius\ShopApiPlugin\Controller\Order;
 
 use Symfony\Component\HttpFoundation\Response;
 use Tests\Sylius\ShopApiPlugin\Controller\JsonApiTestCase;
+use Tests\Sylius\ShopApiPlugin\Controller\Utils\OrderPlacerTrait;
 use Tests\Sylius\ShopApiPlugin\Controller\Utils\ShopUserLoginTrait;
 
 final class OrderUpdatePaymentMethodApiTest extends JsonApiTestCase
 {
     use ShopUserLoginTrait;
+    use OrderPlacerTrait;
 
     /**
      * @test
      */
     public function it_allows_to_update_payment_method(): void
     {
-        $fixtures = $this->loadFixturesFromFiles(['customer.yml', 'country.yml', 'address.yml', 'shop.yml', 'payment.yml', 'shipping.yml', 'order.yml']);
-        $this->logInUser('oliver@queen.com', '123password');
+        $this->loadFixturesFromFiles(['customer.yml', 'country.yml', 'address.yml', 'shop.yml', 'payment.yml', 'shipping.yml']);
+        $token = 'ORDERTOKENPLACED';
+        $email = 'oliver@queen.com';
+
+        $this->logInUser($email, '123password');
+
+        $this->placeOrderForCustomerWithEmail($email, $token);
 
         $data =
 <<<EOT
@@ -27,7 +34,7 @@ final class OrderUpdatePaymentMethodApiTest extends JsonApiTestCase
         }
 EOT;
 
-        $this->client->request('PUT', $this->getPaymentUrl('ORDERTOKENPLACED2') . '/0', [], [], self::CONTENT_TYPE_HEADER, $data);
+        $this->client->request('PUT', $this->getPaymentUrl('ORDERTOKENPLACED') . '/0', [], [], self::CONTENT_TYPE_HEADER, $data);
 
         $response = $this->client->getResponse();
         $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
@@ -38,8 +45,13 @@ EOT;
      */
     public function it_does_not_allow_to_update_payment_method_on_paid_order(): void
     {
-        $fixtures = $this->loadFixturesFromFiles(['customer.yml', 'country.yml', 'address.yml', 'shop.yml', 'payment.yml', 'shipping.yml', 'order.yml']);
-        $this->logInUser('oliver@queen.com', '123password');
+        $this->loadFixturesFromFiles(['customer.yml', 'country.yml', 'address.yml', 'shop.yml', 'payment.yml', 'shipping.yml']);
+        $token = 'ORDERTOKENPAID';
+        $email = 'oliver@queen.com';
+
+        $this->logInUser($email, '123password');
+
+        $this->placeOrderForCustomerWithEmail($email, $token);
 
         $data =
 <<<EOT
