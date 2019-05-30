105bc731e7a334c7f6518c99bda6acbb0c6aa8d2
mamazu <14860264+mamazu@users.noreply.github.com>
2019-05-30 18:40:26 +0200

    Making the command provider overridable

--- src/CommandProvider/DropCartCommandProvider.php
+++ src/CommandProvider/DropCartCommandProvider.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Sylius\ShopApiPlugin\CommandProvider;
 
-use Sylius\ShopApiPlugin\Command\DropCart;
-use Sylius\ShopApiPlugin\Request\DropCartRequest;
-use Sylius\ShopApiPlugin\Validator\Constraints\CartExists;
+use Sylius\ShopApiPlugin\Command\Cart\DropCart;
+use Sylius\ShopApiPlugin\Request\Cart\DropCartRequest;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-final class DropCartCommandProvider implements CommandProviderInterface
+class DropCartCommandProvider implements CommandProviderInterface
 {
     /** @var ValidatorInterface */
     private $validator;
@@ -22,15 +20,23 @@ final class DropCartCommandProvider implements CommandProviderInterface
         $this->validator = $validator;
     }
 
-    public function validate(Request $request): ConstraintViolationListInterface
+    protected function transformRequest(Request $request): DropCartRequest
     {
-        return $this->validator->validate(new DropCartRequest($request));
+        return new DropCartRequest($request);
     }
 
-    public function getCommand(Request $request): object
+    protected function transformCommand(DropCartRequest $dropCartRequest): DropCart
     {
-        $dropCartRequest = new DropCartRequest($request);
-
         return new DropCart($dropCartRequest->token);
+    }
+
+    final public function validate(Request $request): ConstraintViolationListInterface
+    {
+        return $this->validator->validate($this->transformRequest($request));
+    }
+
+    final public function getCommand(Request $request): object
+    {
+        return $this->transformCommand($this->transformRequest($request));
     }
 }
