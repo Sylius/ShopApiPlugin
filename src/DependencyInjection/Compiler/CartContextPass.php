5c01f901aab8e8449b6f772e2601c5bd86400128
mamazu <14860264+mamazu@users.noreply.github.com>
2019-06-22 11:50:44 +0200

    Fixing double carts

---
+++ src/DependencyInjection/Compiler/CartContextPass.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Sylius\ShopApiPlugin\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+class CartContextPass implements CompilerPassInterface
+{
+    public const CART_CONTEXT_SERVICE_TAG = 'sylius.context.cart';
+
+    private $compositeId = 'sylius.shop_api_plugin.context.cart';
+    private $tagName = self::CART_CONTEXT_SERVICE_TAG;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->has($this->compositeId)) {
+            return;
+        }
+
+        $contextDefinition = $container->findDefinition($this->compositeId);
+
+        $taggedServices = $container->findTaggedServiceIds($this->tagName);
+        foreach ($taggedServices as $id => $tags) {
+            $this->addMethodCalls($contextDefinition, $id, $tags);
+        }
+    }
+
+    private function addMethodCalls(Definition $contextDefinition, string $id, array $tags): void
+    {
+        if ($id === 'sylius.context.cart.new') {
+            return;
+        }
+
+        foreach ($tags as $attributes) {
+            $contextDefinition->addMethodCall('addContext', [new Reference($id), $attributes['priority'] ?? 0]);
+        }
+    }
+}
--- src/ShopApiPlugin.php
+++ src/ShopApiPlugin.php
@@ -5,9 +5,18 @@ declare(strict_types=1);
 namespace Sylius\ShopApiPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Sylius\ShopApiPlugin\DependencyInjection\Compiler\CartContextPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class ShopApiPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new CartContextPass());
+    }
 }
