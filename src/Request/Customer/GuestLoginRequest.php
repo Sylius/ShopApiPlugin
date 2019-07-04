0fd3fd173a8844ca84a8c55d26bbbff4b26ef93b
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2019-07-04 11:05:39 +0200

    Added guest login & firewall config

---
+++ src/Request/Customer/GuestLoginRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\ShopApiPlugin\Request\Customer;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class GuestLoginRequest
+{
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var string
+     */
+    protected $orderNumber;
+
+    /**
+     * @var string
+     */
+    protected $paymentMethodCode;
+
+    public function __construct(Request $request)
+    {
+        $this->email             = $request->request->get('email');
+        $this->orderNumber       = $request->request->get('orderNumber');
+        $this->paymentMethodCode = $request->request->get('paymentMethod');
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getOrderNumber(): string
+    {
+        return $this->orderNumber;
+    }
+
+    public function getPaymentMethodCode(): string
+    {
+        return $this->paymentMethodCode;
+    }
+}
