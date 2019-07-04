1b067268af3874c892d916c878855459c032d301
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2019-07-04 11:26:50 +0200

    Added tests

---
+++ tests/Controller/Customer/GuestLoginApiTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\ShopApiPlugin\Controller\Customer;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Sylius\ShopApiPlugin\Controller\JsonApiTestCase;
+
+final class GuestLoginApiTest extends JsonApiTestCase
+{
+    /** @test */
+    public function it_returns_jwt_on_success(): void
+    {
+        $this->loadFixturesFromFiles(['order.yml', 'shop.yml', 'customer.yml', 'address.yml']);
+
+        $data = <<<EOT
+        {
+            "email": "john@doe.com",
+            "orderNumber": "00000078",
+            "paymentMethod": "bank_payment"
+        }
+EOT;
+
+        $this->client->request(Request::METHOD_POST, '/shop-api/WEB_GB/guest/login_check', [], [], self::CONTENT_TYPE_HEADER, $data);
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'customer/guest_login_response');
+    }
+
+    /** @test */
+    public function it_returns_unauthorized_on_wrong_payment_method(): void
+    {
+        $this->loadFixturesFromFiles(['order.yml', 'shop.yml', 'customer.yml', 'address.yml']);
+
+        $data = <<<EOT
+        {
+            "email": "john@doe.com",
+            "orderNumber": "00000078",
+            "paymentMethod": "PayPal"
+        }
+EOT;
+
+        $this->client->request(Request::METHOD_POST, '/shop-api/WEB_GB/guest/login_check', [], [], self::CONTENT_TYPE_HEADER, $data);
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /** @test */
+    public function it_returns_unauthorized_if_order_belongs_to_user(): void
+    {
+        $this->loadFixturesFromFiles(['order.yml', 'shop.yml', 'customer.yml', 'address.yml']);
+
+        $data = <<<EOT
+        {
+            "email": "oliver@queen.com",
+            "orderNumber": "00000022",
+            "paymentMethod": "Bank Payment"
+        }
+EOT;
+
+        $this->client->request(Request::METHOD_POST, '/shop-api/WEB_GB/guest/login_check', [], [], self::CONTENT_TYPE_HEADER, $data);
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_UNAUTHORIZED);
+    }
+}
---
+++ tests/Controller/Order/GuestOrderShowApiTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\ShopApiPlugin\Controller\Order;
+
+
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+use Sylius\ShopApiPlugin\Encoder\GuestOrderJWTEncoderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Sylius\ShopApiPlugin\Controller\JsonApiTestCase;
+
+final class GuestOrderShowApiTest extends JsonApiTestCase
+{
+    const GUEST_TOKEN_HEADER = 'HTTP_Sylius-Guest-Token';
+
+    /** @test */
+    public function it_returns_summary_for_guest_order(): void
+    {
+        $this->loadFixturesFromFiles(['order.yml', 'shop.yml', 'customer.yml', 'address.yml']);
+
+        /** @var GuestOrderJWTEncoderInterface $encoder */
+        $encoder = $this->get('brille24.shop_api.encoder.guest_order_jwtencoder');
+
+        /** @var OrderRepositoryInterface $orderRepository */
+        $orderRepository = $this->get('sylius.repository.order');
+
+        $token = 'GUEST_ORDER_TOKEN';
+        $order = $orderRepository->findOneByTokenValue($token);
+        $jwt   = $encoder->encode($order);
+
+        $this->client->setServerParameter(self::GUEST_TOKEN_HEADER, $jwt);
+        $this->client->request(Request::METHOD_GET, '/shop-api/WEB_GB/guest/order');
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'order/guest_order_summary_response');
+    }
+
+    /** @test */
+    public function it_returns_unauthorized_if_no_jwt_provided(): void
+    {
+        $this->loadFixturesFromFiles(['order.yml', 'shop.yml', 'customer.yml', 'address.yml']);
+
+        $this->client->request(Request::METHOD_GET, '/shop-api/WEB_GB/guest/order');
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_UNAUTHORIZED);
+    }
+
+    /** @test */
+    public function it_returns_unauthorized_if_invalid_jwt_provided(): void
+    {
+        $this->loadFixturesFromFiles(['order.yml', 'shop.yml', 'customer.yml', 'address.yml']);
+
+        $jwt = 'abc';
+
+        $this->client->setServerParameter(self::GUEST_TOKEN_HEADER, $jwt);
+        $this->client->request(Request::METHOD_GET, '/shop-api/WEB_GB/guest/order');
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_UNAUTHORIZED);
+    }
+}
