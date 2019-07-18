0966b2b94ba063e2235c6b11d9f8378491d3dda4
Grzegorz Sadowski <sadowskigp@gmail.com>
2019-07-18 10:24:21 +0200

    Use channel context instead of hostname request resolver

--- spec/Http/RequestBasedLocaleProviderSpec.php
+++ spec/Http/RequestBasedLocaleProviderSpec.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace spec\Sylius\ShopApiPlugin\Http;
 
 use PhpSpec\ObjectBehavior;
-use Sylius\Component\Channel\Context\RequestBased\RequestResolverInterface;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Context\ChannelNotFoundException;
 use Sylius\Component\Core\Model\ChannelInterface;
-use Sylius\ShopApiPlugin\Exception\ChannelNotFoundException;
 use Sylius\ShopApiPlugin\Http\RequestBasedLocaleProviderInterface;
 use Sylius\ShopApiPlugin\Provider\SupportedLocaleProviderInterface;
 use Symfony\Component\HttpFoundation\ParameterBag;
@@ -16,10 +16,10 @@ use Symfony\Component\HttpFoundation\Request;
 final class RequestBasedLocaleProviderSpec extends ObjectBehavior
 {
     function let(
-        RequestResolverInterface $hostnameBasedRequestResolver,
+        ChannelContextInterface $channelContext,
         SupportedLocaleProviderInterface $supportedLocaleProvider
     ): void {
-        $this->beConstructedWith($hostnameBasedRequestResolver, $supportedLocaleProvider);
+        $this->beConstructedWith($channelContext, $supportedLocaleProvider);
     }
 
     function it_implements_request_based_locale_provider_interface(): void
@@ -28,38 +28,38 @@ final class RequestBasedLocaleProviderSpec extends ObjectBehavior
     }
 
     function it_provides_a_locale_code(
-        RequestResolverInterface $hostnameBasedRequestResolver,
+        ChannelContextInterface $channelContext,
         SupportedLocaleProviderInterface $supportedLocaleProvider,
         ChannelInterface $channel,
         Request $request
     ): void {
         $request->query = new ParameterBag(['locale' => 'fr_FR']);
 
-        $hostnameBasedRequestResolver->findChannel($request)->willReturn($channel);
+        $channelContext->getChannel()->willReturn($channel);
         $supportedLocaleProvider->provide('fr_FR', $channel)->willReturn('fr_FR');
 
         $this->getLocaleCode($request)->shouldReturn('fr_FR');
     }
 
     function it_provides_a_locale_code_even_if_it_is_not_defined_explicitly(
-        RequestResolverInterface $hostnameBasedRequestResolver,
+        ChannelContextInterface $channelContext,
         SupportedLocaleProviderInterface $supportedLocaleProvider,
         ChannelInterface $channel,
         Request $request
     ): void {
         $request->query = new ParameterBag([]);
 
-        $hostnameBasedRequestResolver->findChannel($request)->willReturn($channel);
+        $channelContext->getChannel()->willReturn($channel);
         $supportedLocaleProvider->provide(null, $channel)->willReturn('en_US');
 
         $this->getLocaleCode($request)->shouldReturn('en_US');
     }
 
     function it_throws_an_exception_if_channel_cannot_be_resolved(
-        RequestResolverInterface $hostnameBasedRequestResolver,
+        ChannelContextInterface $channelContext,
         Request $request
     ): void {
-        $hostnameBasedRequestResolver->findChannel($request)->willReturn(null);
+        $channelContext->getChannel()->willThrow(ChannelNotFoundException::class);
 
         $this
             ->shouldThrow(ChannelNotFoundException::class)
--- src/Http/RequestBasedLocaleProvider.php
+++ src/Http/RequestBasedLocaleProvider.php
@@ -4,36 +4,33 @@ declare(strict_types=1);
 
 namespace Sylius\ShopApiPlugin\Http;
 
-use Sylius\Component\Channel\Context\RequestBased\RequestResolverInterface;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Channel\Context\ChannelNotFoundException;
 use Sylius\Component\Core\Model\ChannelInterface;
-use Sylius\ShopApiPlugin\Exception\ChannelNotFoundException;
 use Sylius\ShopApiPlugin\Provider\SupportedLocaleProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 final class RequestBasedLocaleProvider implements RequestBasedLocaleProviderInterface
 {
-    /** @var RequestResolverInterface */
-    private $hostnameBasedRequestResolver;
+    /** @var ChannelContextInterface */
+    private $channelContext;
 
     /** @var SupportedLocaleProviderInterface */
     private $supportedLocaleProvider;
 
     public function __construct(
-        RequestResolverInterface $hostnameBasedRequestResolver,
+        ChannelContextInterface $channelContext,
         SupportedLocaleProviderInterface $supportedLocaleProvider
     ) {
-        $this->hostnameBasedRequestResolver = $hostnameBasedRequestResolver;
+        $this->channelContext = $channelContext;
         $this->supportedLocaleProvider = $supportedLocaleProvider;
     }
 
     /** @throws ChannelNotFoundException */
     public function getLocaleCode(Request $request): string
     {
-        /** @var ChannelInterface|null $channel */
-        $channel = $this->hostnameBasedRequestResolver->findChannel($request);
-        if (null === $channel) {
-            throw ChannelNotFoundException::occur();
-        }
+        /** @var ChannelInterface $channel */
+        $channel = $this->channelContext->getChannel();
 
         return $this->supportedLocaleProvider->provide($request->query->get('locale'), $channel);
     }
