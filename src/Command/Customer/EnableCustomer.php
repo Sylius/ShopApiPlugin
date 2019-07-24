0790c9e88bec3e76a43221e056fa2dcbd5b0eaf2
Łukasz Chruściel <lchrusciel@gmail.com>
2019-07-24 10:15:23 +0200

    [Customer] Extract customer enabling logic to custom handler

---
+++ src/Command/Customer/EnableCustomer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\ShopApiPlugin\Command\Customer;
+
+class EnableCustomer
+{
+    /** @var string */
+    protected $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function email(): string
+    {
+        return $this->email;
+    }
+}
