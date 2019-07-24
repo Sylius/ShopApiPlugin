bf722788d45e8553960e33e22459e7dcc2a3893d
Łukasz Chruściel <lchrusciel@gmail.com>
2019-07-24 11:02:55 +0200

    [Cart] Simplify cart recalculation after customer cart assignment

--- src/EventListener/CartBlamerListener.php
+++ src/EventListener/CartBlamerListener.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Sylius\ShopApiPlugin\EventListener;
 
-use Doctrine\Common\Persistence\ObjectManager;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
 use Sylius\Component\Core\Model\ShopUserInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
-use Sylius\Component\Order\Processor\OrderProcessorInterface;
 use Sylius\ShopApiPlugin\Command\Cart\AssignCustomerToCart;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -16,32 +14,22 @@ use Webmozart\Assert\Assert;
 
 final class CartBlamerListener
 {
-    /** @var ObjectManager */
-    private $cartManager;
-
     /** @var OrderRepositoryInterface */
     private $cartRepository;
 
     /** @var MessageBusInterface */
     private $bus;
 
-    /** @var OrderProcessorInterface */
-    private $orderProcessor;
-
     /** @var RequestStack */
     private $requestStack;
 
     public function __construct(
-        ObjectManager $cartManager,
         OrderRepositoryInterface $cartRepository,
         MessageBusInterface $bus,
-        OrderProcessorInterface $orderProcessor,
         RequestStack $requestStack
     ) {
-        $this->cartManager = $cartManager;
         $this->cartRepository = $cartRepository;
         $this->bus = $bus;
-        $this->orderProcessor = $orderProcessor;
         $this->requestStack = $requestStack;
     }
 
@@ -70,10 +58,5 @@ final class CartBlamerListener
                 $user->getCustomer()->getEmail()
             )
         );
-
-        $this->orderProcessor->process($cart);
-
-        $this->cartManager->persist($cart);
-        $this->cartManager->flush();
     }
 }
--- src/Handler/Cart/AssignCustomerToCartHandler.php
+++ src/Handler/Cart/AssignCustomerToCartHandler.php
@@ -6,6 +6,7 @@ namespace Sylius\ShopApiPlugin\Handler\Cart;
 
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+use Sylius\Component\Order\Processor\OrderProcessorInterface;
 use Sylius\ShopApiPlugin\Command\Cart\AssignCustomerToCart;
 use Sylius\ShopApiPlugin\Provider\CustomerProviderInterface;
 use Webmozart\Assert\Assert;
@@ -13,26 +14,35 @@ use Webmozart\Assert\Assert;
 final class AssignCustomerToCartHandler
 {
     /** @var OrderRepositoryInterface */
-    private $orderRepository;
+    private $cartRepository;
+
+    /** @var OrderProcessorInterface */
+    private $orderProcessor;
 
     /** @var CustomerProviderInterface */
     private $customerProvider;
 
-    public function __construct(OrderRepositoryInterface $orderRepository, CustomerProviderInterface $customerProvider)
-    {
-        $this->orderRepository = $orderRepository;
+    public function __construct(
+        OrderRepositoryInterface $cartRepository,
+        OrderProcessorInterface $orderProcessor,
+        CustomerProviderInterface $customerProvider
+    ) {
+        $this->cartRepository = $cartRepository;
         $this->customerProvider = $customerProvider;
+        $this->orderProcessor = $orderProcessor;
     }
 
     public function __invoke(AssignCustomerToCart $assignOrderToCustomer): void
     {
-        /** @var OrderInterface $order */
-        $order = $this->orderRepository->findOneBy(['tokenValue' => $assignOrderToCustomer->orderToken()]);
+        /** @var OrderInterface $cart */
+        $cart = $this->cartRepository->findOneBy(['tokenValue' => $assignOrderToCustomer->orderToken()]);
 
-        Assert::notNull($order, sprintf('Order with %s token has not been found.', $assignOrderToCustomer->orderToken()));
+        Assert::notNull($cart, sprintf('Order with %s token has not been found.', $assignOrderToCustomer->orderToken()));
 
         $customer = $this->customerProvider->provide($assignOrderToCustomer->email());
 
-        $order->setCustomer($customer);
+        $cart->setCustomer($customer);
+
+        $this->orderProcessor->process($cart);
     }
 }
