be528c4b746fdb54ebef975a18f8b7540952aab1
DenisDev <denis.kovacevic@jobcloud.ch>
2019-09-17 16:33:27 +0200

    fix: return array for product select attribute type, improve testing with select attributes for different languages

--- spec/Factory/Product/ProductAttributeValueViewFactorySpec.php
+++ spec/Factory/Product/ProductAttributeValueViewFactorySpec.php
@@ -56,14 +56,55 @@ final class ProductAttributeValueViewFactorySpec extends ObjectBehavior
         $productAttributeValue->getAttribute()->willReturn($productAttribute);
 
         $productAttribute->getTranslation('en_GB')->willReturn($productAttributeTranslation);
-        $productAttribute->getConfiguration()->willReturn(['choices' => ['1' => 'Nice, shinny certificate.']]);
+        $productAttribute->getConfiguration()->willReturn([
+            'choices' => [
+                '1' => [
+                    'en_GB' => 'Nice, shinny certificate.',
+                ],
+            ],
+        ]);
 
         $productAttributeTranslation->getName()->willReturn('Certificate XPTO');
 
         $productAttributeValueView = new ProductAttributeValueView();
         $productAttributeValueView->code = 'CERTIFICATE_1';
         $productAttributeValueView->name = 'Certificate XPTO';
-        $productAttributeValueView->value = 'Nice, shinny certificate.';
+        $productAttributeValueView->value = ['Nice, shinny certificate.'];
+
+        $this->create($productAttributeValue, 'en_GB')->shouldBeLike($productAttributeValueView);
+    }
+
+    function it_creates_product_attribute_value_view_for_select_attribute_type_for_multiple_choices(
+        ProductAttributeValueInterface $productAttributeValue,
+        ProductAttributeInterface $productAttribute,
+        ProductAttributeTranslationInterface $productAttributeTranslation
+    ) {
+        $productAttributeValue->getCode()->willReturn('CERTIFICATES');
+        $productAttributeValue->getValue()->willReturn(['1', '2']);
+        $productAttributeValue->getType()->willReturn('select');
+        $productAttributeValue->getAttribute()->willReturn($productAttribute);
+
+        $productAttribute->getTranslation('en_GB')->willReturn($productAttributeTranslation);
+        $productAttribute->getConfiguration()->willReturn([
+            'choices' => [
+                '1' => [
+                    'en_GB' => 'Certified Software Developer Program.',
+                ],
+                '2' => [
+                    'en_GB' => 'Certified Software Tester.',
+                ],
+            ],
+        ]);
+
+        $productAttributeTranslation->getName()->willReturn('IT Certificats');
+
+        $productAttributeValueView = new ProductAttributeValueView();
+        $productAttributeValueView->code = 'CERTIFICATES';
+        $productAttributeValueView->name = 'IT Certificats';
+        $productAttributeValueView->value = [
+            'Certified Software Developer Program.',
+            'Certified Software Tester.',
+        ];
 
         $this->create($productAttributeValue, 'en_GB')->shouldBeLike($productAttributeValueView);
     }
