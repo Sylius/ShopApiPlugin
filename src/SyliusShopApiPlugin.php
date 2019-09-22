6563c5831b7ab6135275aa0ae2a7bdf1311353b3
Łukasz Chruściel <lchrusciel@gmail.com>
2019-09-22 12:44:04 +0200

    [Performance] Add back cached channel context

--- src/SyliusShopApiPlugin.php
+++ src/SyliusShopApiPlugin.php
@@ -5,18 +5,9 @@ declare(strict_types=1);
 namespace Sylius\ShopApiPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
-use Sylius\ShopApiPlugin\DependencyInjection\Compiler\ChannelContextPass;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class SyliusShopApiPlugin extends Bundle
 {
     use SyliusPluginTrait;
-
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new ChannelContextPass());
-    }
 }
