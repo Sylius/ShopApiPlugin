39f3d8ad581e326b17acc0ad96fac887dc12cdeb
CSchulz <christian@schulz.re>
2019-09-23 18:22:03 +0200

    add additional tests

--- tests/Controller/Product/ShowCatalogByPhraseApiTest.php
+++ tests/Controller/Product/ShowCatalogByPhraseApiTest.php
@@ -7,7 +7,7 @@ namespace Tests\Sylius\ShopApiPlugin\Controller\Product;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\Sylius\ShopApiPlugin\Controller\JsonApiTestCase;
 
-final class ShowCatalogByNameApiTest extends JsonApiTestCase
+final class ShowCatalogByPhraseApiTest extends JsonApiTestCase
 {
     /**
      * @test
@@ -20,6 +20,19 @@ final class ShowCatalogByNameApiTest extends JsonApiTestCase
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/product_list_page_by_phrase_mug_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_empty_products_list_for_non_existing_products(): void
+    {
+        $this->loadFixturesFromFiles(['shop.yml']);
+
+        $this->client->request('GET', '/shop-api/products/by-phrase/unknown', [], [], self::CONTENT_TYPE_HEADER);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/product_list_page_by_phrase_empty_response', Response::HTTP_OK);
     }
 
     /**
@@ -72,5 +85,16 @@ final class ShowCatalogByNameApiTest extends JsonApiTestCase
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'product/product_list_page_by_phrase_mug_in_german_including_description_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_throws_a_not_found_exception_if_channel_has_not_been_found(): void
+    {
+        $this->client->request('GET', '/shop-api/products/by-phrase/mug', [], [], self::CONTENT_TYPE_HEADER);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'product/channel_has_not_been_found_response', Response::HTTP_NOT_FOUND);
     }
 }
