8bbd22caf1c8e8097ffff876454a07a71791678c
mamazu <14860264+mamazu@users.noreply.github.com>
2019-10-10 19:36:03 +0200

    Adding codestyle

--- src/Mailer/Emails.php
+++ src/Mailer/Emails.php
@@ -7,6 +7,7 @@ namespace Sylius\ShopApiPlugin\Mailer;
 final class Emails
 {
     public const EMAIL_VERIFICATION_TOKEN = 'api_verification_token';
+
     public const EMAIL_RESET_PASSWORD_TOKEN = 'api_reset_password_token';
 
     private function __construct()
