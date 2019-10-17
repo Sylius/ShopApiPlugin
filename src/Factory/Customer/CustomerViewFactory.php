2c56cf7e622f4d4e6ce74d8736c916b6868d26df
Александр Калинин <phex13@mail.ru>
2019-10-17 21:55:03 +0300

    TEA-48 | the ability to add avatars to users profiles

--- src/Factory/Customer/CustomerViewFactory.php
+++ src/Factory/Customer/CustomerViewFactory.php
@@ -5,16 +5,23 @@ declare(strict_types=1);
 namespace Sylius\ShopApiPlugin\Factory\Customer;
 
 use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\Model\ImageInterface;
+use Sylius\ShopApiPlugin\Factory\ImageViewFactory;
 use Sylius\ShopApiPlugin\View\Customer\CustomerView;
 
 final class CustomerViewFactory implements CustomerViewFactoryInterface
 {
+
     /** @var string */
     private $customerViewClass;
 
-    public function __construct(string $customerViewClass)
+    /** @var ImageViewFactory */
+    private $imageViewFactory;
+
+    public function __construct(string $customerViewClass, ImageViewFactory $imageViewFactory)
     {
         $this->customerViewClass = $customerViewClass;
+        $this->imageViewFactory  = $imageViewFactory;
     }
 
     /** {@inheritdoc} */
@@ -23,16 +30,19 @@ final class CustomerViewFactory implements CustomerViewFactoryInterface
         /** @var CustomerView $customerView */
         $customerView = new $this->customerViewClass();
 
-        $customerView->id = $customer->getId();
-        $customerView->firstName = $customer->getFirstName();
-        $customerView->lastName = $customer->getLastName();
-        $customerView->email = $customer->getEmail();
-        $customerView->birthday = $customer->getBirthday();
-        $customerView->gender = $customer->getGender();
-        $customerView->phoneNumber = $customer->getPhoneNumber();
+        $customerView->id                     = $customer->getId();
+        $customerView->firstName              = $customer->getFirstName();
+        $customerView->lastName               = $customer->getLastName();
+        $customerView->email                  = $customer->getEmail();
+        $customerView->birthday               = $customer->getBirthday();
+        $customerView->gender                 = $customer->getGender();
+        $customerView->phoneNumber            = $customer->getPhoneNumber();
         $customerView->subscribedToNewsletter = $customer->isSubscribedToNewsletter();
-        if($customer->getUser()){
-            $customerView->avatar = getenv('AVS_S3_CDN') . $customer->getUser()->getAvatar()->getPath();
+        if ($customer->getUser()) {
+            /** @var ImageInterface $image */
+            $image = $customer->getUser()->getAvatar();
+
+            $customerView->avatar = $this->imageViewFactory->create($image);
         }
 
         return $customerView;
