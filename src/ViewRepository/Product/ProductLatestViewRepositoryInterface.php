ddf58fe4e661e420746a74ab17c712eabea050a5
Александр Калинин <phex13@mail.ru>
2019-10-30 00:15:16 +0300

    TEA-64 | added pagination to latest products

--- src/ViewRepository/Product/ProductLatestViewRepositoryInterface.php
+++ src/ViewRepository/Product/ProductLatestViewRepositoryInterface.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Sylius\ShopApiPlugin\ViewRepository\Product;
 
-use Sylius\ShopApiPlugin\View\Product\ProductListView;
+use Sylius\ShopApiPlugin\Model\PaginatorDetails;
+use Sylius\ShopApiPlugin\View\Product\PageView;
 
 interface ProductLatestViewRepositoryInterface
 {
-    public function getLatestProducts(string $channelCode, ?string $localeCode, int $count): ProductListView;
+    public function getLatestProducts(string $channelCode, ?string $localeCode, PaginatorDetails $paginatorDetails): PageView;
 }
