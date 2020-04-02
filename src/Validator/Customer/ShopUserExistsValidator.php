c8a73db920f921a95f3a41c1b96c3671c38cd3be
alexander.kalinin <phex13@mail.ru>
2020-04-02 20:16:43 +0300

    TWS-420 | translator added

--- src/Validator/Customer/ShopUserExistsValidator.php
+++ src/Validator/Customer/ShopUserExistsValidator.php
@@ -7,22 +7,28 @@ namespace Sylius\ShopApiPlugin\Validator\Customer;
 use Sylius\Component\User\Repository\UserRepositoryInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class ShopUserExistsValidator extends ConstraintValidator
 {
 
     /** @var UserRepositoryInterface */
     private $userRepository;
+    /** @var TranslatorInterface  */
+    private $translator;
 
-    public function __construct(UserRepositoryInterface $userRepository)
-    {
+    public function __construct(
+        UserRepositoryInterface $userRepository,
+        TranslatorInterface $translator
+    ) {
         $this->userRepository = $userRepository;
+        $this->translator     = $translator;
     }
 
     public function validate($string, Constraint $constraint)
     {
         if (null === $string || null === $this->userRepository->findOneByEmail($string)) {
-            $this->context->addViolation($constraint->message);
+            $this->context->addViolation($this->translator->trans($constraint->message));
         }
     }
 }
