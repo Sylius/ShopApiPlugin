78f879e1713ca7b41436cd89415194d4b340ca38
iicux <stalker7600@gmail.com>
2020-06-08 14:22:52 +0300

    TWS-592 - если вылезет эксепшн на неверный номер поля, то тоже вывалится ошибка валидации

--- src/Validator/Address/PhoneValidator.php
+++ src/Validator/Address/PhoneValidator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Sylius\ShopApiPlugin\Validator\Address;
 
 use libphonenumber\PhoneNumberType;
+use libphonenumber\PhoneNumberUtil;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
@@ -22,10 +23,14 @@ final class PhoneValidator extends ConstraintValidator
 
     public function validate($request, Constraint $constraint)
     {
-        $phoneUtil        = \libphonenumber\PhoneNumberUtil::getInstance();
-        $swissNumberProto = $phoneUtil->parse($request, "CH");
-        $valid            = $phoneUtil->isValidNumber($swissNumberProto);
-        if (! ($valid && $phoneUtil->getNumberType($swissNumberProto) == PhoneNumberType::MOBILE)) {
+        try {
+            $phoneUtil        = PhoneNumberUtil::getInstance();
+            $swissNumberProto = $phoneUtil->parse($request, "RU");
+            $valid            = $phoneUtil->isValidNumber($swissNumberProto);
+        } catch (\Exception $e) {
+            return $this->context->addViolation($constraint->message);
+        }
+        if (!($valid && $phoneUtil->getNumberType($swissNumberProto) == PhoneNumberType::MOBILE)) {
             return $this->context->addViolation($constraint->message);
         }
     }
