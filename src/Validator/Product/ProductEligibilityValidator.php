d2984e12d18696c3a654579896605ab3e86069c0
Nenad Stefanovic <nenadstefanovic@MacBook-Pro.local>
2020-11-06 15:48:32 +0100

    Added product eligibility validation

---
+++ src/Validator/Product/ProductEligibilityValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\ShopApiPlugin\Validator\Product;
+
+use Sylius\Component\Core\Repository\ProductRepositoryInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+final class ProductEligibilityValidator extends ConstraintValidator
+{
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    /** {@inheritdoc} */
+    public function validate($productCode, Constraint $constraint): void
+    {
+        if (null === $productCode) {
+            return;
+        }
+
+        $product = $this->productRepository->findOneByCode($productCode);
+
+        if (null === $product || $product->isEnabled()) {
+            return;
+        }
+
+        $this->context->addViolation($constraint->message);
+    }
+}
