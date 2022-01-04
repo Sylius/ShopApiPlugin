f1bc0c96b9c0638403ebdff935f2022dcc529f0f
Mohamed Thiam <medteck15@gmail.com>
2022-01-04 15:10:06 +0000

    Adds /checkout/countries & /checkout/countries/{code} endpoints to get list of countries + country details by code

--- tests/Controller/Checkout/CountriesApiTest.php
+++ tests/Controller/Checkout/CountriesApiTest.php
@@ -4,7 +4,48 @@
 namespace Tests\Sylius\ShopApiPlugin\Controller\Checkout;
 
 
-class CountriesApiTest
+use Symfony\Component\HttpFoundation\Response;
+use Tests\Sylius\ShopApiPlugin\Controller\JsonApiTestCase;
+
+class CountriesApiTest extends JsonApiTestCase
 {
 
+    /**
+     * @test
+     */
+    public function it_shows_list_of_all_countries(): void
+    {
+        $this->loadFixturesFromFiles(['country.yml']);
+
+        $this->client->request('GET', '/shop-api/checkout/countries', [], [], self::CONTENT_TYPE_HEADER);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'checkout/all_countries_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_shows_country_details_by_code(): void
+    {
+        $this->loadFixturesFromFiles(['country.yml']);
+
+        $this->client->request('GET', '/shop-api/checkout/countries/GB', [], [], self::CONTENT_TYPE_HEADER);
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'checkout/one_of_countries_response', Response::HTTP_OK);
+    }
+
+    /**
+     * @test
+     */
+    public function it_fails_with_404_when_country_code_does_not_exist(): void
+    {
+        $this->loadFixturesFromFiles(['country.yml']);
+
+        $this->client->request('GET', '/shop-api/checkout/countries/XX', [], [], self::CONTENT_TYPE_HEADER);
+        $response = $this->client->getResponse();
+
+        $this->assertResponseCode($response, Response::HTTP_NOT_FOUND);
+    }
 }
