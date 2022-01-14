486e3f65eb78a882ff07c6991d4efb9756af0324
Mohamed Thiam <medteck15@gmail.com>
2022-01-14 12:22:34 +0000

    Adds strict types declaration to CountriesApiTest

--- tests/Controller/Checkout/CountriesApiTest.php
+++ tests/Controller/Checkout/CountriesApiTest.php
@@ -1,8 +1,8 @@
 <?php
 
+declare(strict_types=1);
 
 namespace Tests\Sylius\ShopApiPlugin\Controller\Checkout;
-
 
 use Symfony\Component\HttpFoundation\Response;
 use Tests\Sylius\ShopApiPlugin\Controller\JsonApiTestCase;
